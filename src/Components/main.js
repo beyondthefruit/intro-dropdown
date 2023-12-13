6a894599a3033f9b76549e1b8f71bbcb8779338c
Kevin Hanard <kevin.hanard@gmail.com>
2023-12-13 17:23:47 +1100

    feat: website design

--- src/Components/main.js
+++ src/Components/main.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 const Main = () => {
   return (
-    <>
-      <h1> Make remote work great again</h1>
-      <p>
-        Get your team in sync, no matter your location. Streamline processes,
-        create team rituals, and watch productivity soar.
-      </p>
-      <button> Learn more</button>
-    </>
+    <article>
+      <div className='text'>
+        {' '}
+        <h1> Make remote work great again</h1>
+        <p>
+          Get your team in sync, no matter your location. Streamline processes,
+          create team rituals, and watch productivity soar.
+        </p>
+        <button> Learn more</button>
+      </div>
+      <div className='photo'>
+        <img src={require('../assets/image-hero-desktop.png')} alt='laptop' />
+      </div>
+    </article>
   );
 };
 
--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Logo from '../assets/logo.svg';
 const NavMenu = () => {
   return (
-    <>
-      <p>snap</p>
-      <p>Features Company Careers About</p>
+    <header>
+      <div className='logo'>
+        <img src={Logo} alt='snap' />
+      </div>
+      <ul className='drop'>
+        <li className='lili'>Features </li>
+        <li className='lili'>Company </li>
+        <li className='lili'>Careers </li>
+        <li className='lili'>About</li>
+      </ul>
+
       <p>Login Register</p>
-    </>
+    </header>
   );
 };
 
