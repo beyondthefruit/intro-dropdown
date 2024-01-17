b97df5f6a811defe538e31676765d8b8ecb5137e
Kevin Hanard <kevin.hanard@gmail.com>
2024-01-17 11:34:25 +1100

    feat: navbar positin element

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -6,13 +6,13 @@ import { links } from '../data';
 const NavMenu = () => {
   return (
     <header>
-      <nav class='navbar navbar-expand-lg bg-body-tertiary'>
-        <div class='container-fluid'>
-          <a class='navbar-brand' href='#'>
+      <nav className='navbar navbar-expand-lg bg-white-body-tertiary'>
+        <div className='container-fluid'>
+          <a className='navbar-brand' href='#'>
             <img src={Logo} alt='snap' />
           </a>
           <button
-            class='navbar-toggler'
+            className='navbar-toggler'
             type='button'
             data-bs-toggle='collapse'
             data-bs-target='#navbarSupportedContent'
@@ -20,18 +20,19 @@ const NavMenu = () => {
             aria-expanded='false'
             aria-label='Toggle navigation'
           >
-            <span class='navbar-toggler-icon'></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-            <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
               {links.map((link) => {
                 const { id, url, text, sublinks } = link;
                 return (
                   <>
+                    {/* we check if there are sublinks or not, if yes we display a dropdown, if not a li a */}
                     {sublinks ? (
-                      <li class='nav-item dropdown'>
+                      <li className='nav-item dropdown'>
                         <a
-                          class='nav-link dropdown-toggle'
+                          className='nav-link dropdown-toggle'
                           href='#'
                           role='button'
                           data-bs-toggle='dropdown'
@@ -55,9 +56,9 @@ const NavMenu = () => {
                         </ul>
                       </li>
                     ) : (
-                      <li class='nav-item'>
+                      <li className='nav-item'>
                         <a
-                          class='nav-link active'
+                          className='nav-link active'
                           aria-current='page'
                           href={url}
                         >
@@ -69,59 +70,21 @@ const NavMenu = () => {
                 );
               })}
             </ul>
+            <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
+              <li className='nav-item sign justify-content-end me-auto'>
+                <a className='nav-link sign me-auto' href='#'>
+                  Login
+                </a>
+              </li>
+              <li className='nav-item sign justify-content-end me-auto'>
+                <a className='nav-link sign me-auto' href='#'>
+                  Register
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
-
-      {/* <div className='logo'>
-        <img src={Logo} alt='snap' />
-      </div>
-
-      <div className='dropdown-Menu'>
-        {links.map((link) => {
-          const { id, url, text, sublinks } = link;
-          return (
-            <>
-              {sublinks ? (
-                <div className='dropdown'>
-                  <button
-                    className='btn btn-secondary dropdown-toggle'
-                    type='button'
-                    data-bs-toggle='dropdown'
-                    aria-expanded='false'
-                  >
-                    {text}
-                  </button>
-                  <ul className='dropdown-menu'>
-                    {sublinks && (
-                      <li>
-                        {sublinks.map((sublink) => {
-                          const { id, url, text } = sublink;
-                          return (
-                            <a a className='dropdown-item' href={url}>
-                              {text}
-                            </a>
-                          );
-                        })}
-                      </li>
-                    )}
-                  </ul>
-                </div>
-              ) : (
-                <ul>
-                  <li>
-                    <a className='nav-item' href={url}>
-                      {text}
-                    </a>
-                  </li>
-                </ul>
-              )}
-            </>
-          );
-        })}
-      </div>
-
-      <p>Login Register</p> */}
     </header>
   );
 };
